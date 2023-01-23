8472ffd8eb25688f4cbf1f431e1f4010531307d4
Pablo Nguyen <81577310+pnguy150@users.noreply.github.com>
2023-01-23 11:30:16 -0800

    Update PowerDot.java

--- src/PowerDot.java
+++ src/PowerDot.java
@@ -1,2 +1,24 @@
-public class PowerDot {
+import java.awt.*;
+
+//code for the Powerdot Class
+/**
+ *@author Pablo Nguyen
+ * @version 1.0
+ * @since 2023-23-01
+ */
+
+public class PowerDot implements Drawable{
+    private int PositionX;
+    private int PositionY;
+
+    public PowerDot(int x, int y){
+        PositionX = x;
+        PositionY = y;
+    }
+
+    public void draw(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(PositionX,PositionY,20,20);
+    }
+
 }

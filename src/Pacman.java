b1382434b6b1186d1f0f4b04e6006d8ad151eddb
Nathan Ho <97061077+lilnho@users.noreply.github.com>
2023-01-22 19:40:16 -0800

    Add files via upload
    Updated Pacman

--- src/Pacman.java
+++ src/Pacman.java
@@ -1,2 +1,37 @@
-public class Pacman {
+import java.awt.*;
+
+/**
+ * Pacman Class
+ *
+ * @author Nathan Ho
+ */
+public class Pacman implements Drawable{
+    private int positionX;
+    private int positionY;
+
+    public void draw(Graphics go) {
+        go.setColor(Color.YELLOW);
+        go.fillRect(positionX, positionY, 20, 20);
+
+        if(positionX > 400) positionX = 400;
+        if(positionY > 400) positionY = 400;
+        if(positionX < 0)   positionX = 0;
+        if(positionY < 0)   positionY = 0;
+    }
+
+    public void up() {
+        positionY -= 20;
+    }
+
+    public void down() {
+        positionY += 20;
+    }
+
+    public void left() {
+        positionX -= 20;
+    }
+
+    public void right() {
+        positionX += 20;
+    }
 }
